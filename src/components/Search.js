e2823b1da4cfc610477574fa86ceff4eed3fbbff
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-17 20:51:06 -0500

    fix lint

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,23 +7,23 @@ function Search() {
   const history = useHistory();
   return (
     <div className="hero">
-      <div style={{height: "200px"}}></div>
+      <div style={{height: '200px'}}></div>
       <h1 className="hero--title">Lucas & Karen</h1>
       <h2 className="hero--sub-title">8/16/2022</h2>
       <Formik
         initialValues={{
           registrationKey: ''
         }}
-        onSubmit={values => {
+        onSubmit={(values) => {
           setRegistrationKey(values.registrationKey);
           history.push(`/registration`);
         }}
       >
-        {formik => (
+        {(formik) => (
           <Form>
             <div style={{
-              display: "flex",
-              justifyContent: "center"
+              display: 'flex',
+              justifyContent: 'center'
             }}>
               <input
                 className="registration-key-search"
@@ -35,7 +35,7 @@ function Search() {
         )}
       </Formik>
       <div className="hero--blur"></div>
-      {/*</Container>*/}
+      {/* </Container>*/}
     </div>
   );
 }
