76820120d4c088c107726645e02ca0c03939255c
Lucas Desouza <lucasfds2777@gmail.com>
2022-11-19 13:39:48 -0600

    feat: version and changelog link

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Typography} from '@mui/material';
 import LanguagePicker from './LanguagePicker';
+import {getAppInfo} from '../../config';
 
 const Footer = () => {
   const year = new Date().getFullYear();
+  const version = `v${getAppInfo().version}`;
+  const changelog = `https://github.com/LucasCarioca/wedding-registration-guest/releases/tag/${version}`;
   return (<section className={'footer'}>
     <Typography className={'footer--attribution'}>Copyright &copy; Lucas De Souza {year}</Typography>
+    <Typography className={'footer--attribution'}><a href={changelog}>{version}</a></Typography>
     <Typography className={'footer--attribution'} style={{fontSize: '.75rem'}}>Anter Font made from <a href="http://www.onlinewebfonts.com">Online Web Fonts</a> is licensed by CC BY 3.0</Typography>
     <Typography className={'footer--languages'}>
       <LanguagePicker/>
--- src/config.js
+++ src/config.js
@@ -6,6 +6,13 @@ export function getHost() {
   return process.env.REACT_APP_REGISTRAION_API_HOST ?? 'http://localhost:8081';
 }
 
+export function getAppInfo() {
+  return {
+    version: process.env.REACT_APP_VERSION ?? '0.0.0',
+    name: process.env.REACT_APP_NAME ?? 'App',
+  }
+}
+
 export function setRegistrationKey(registrationKey) {
   localStorage.setItem('registration_key', registrationKey);
 }
