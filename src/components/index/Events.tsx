edc631df4fa228659b5e2f82809bdbf0dd8ed578
Lucas Desouza <lucasfds2777@gmail.com>
2023-03-05 14:52:04 -0600

    fix: bug hiding time on farewell party

--- src/components/index/Events.tsx
+++ src/components/index/Events.tsx
@@ -1,15 +1,15 @@
-import { Typography } from "@mui/material"
-import { useRouter } from "next/router"
-import { useDict } from "../../lang"
+import {Typography} from "@mui/material"
+import {useRouter} from "next/router"
+import {useDict} from "../../lang"
 import SectionDivider from "../SectionDivider"
 
 export default function Events() {
-    const { locale } = useRouter()
+    const {locale} = useRouter()
     const dict = useDict(locale)
     return (
         <section id={'events'}>
             <Typography className={'home-section-title'} variant={'h2'}>{dict.eventsTitle}</Typography>
-            <SectionDivider />
+            <SectionDivider/>
             {dict.events.map((event, i) => (<div key={i}>
                 <Typography className={'home-section-sub-title'} variant={'h4'}>
                     {event.title}
@@ -17,25 +17,19 @@ export default function Events() {
                 <Typography className={'event-section-main-paragraph'}>
                     {event.date}
                 </Typography>
-                {event.locationName.length > 0 ? (<>
-                    <Typography className={'event-section-main-paragraph'}>
-                        {event.time}
-                    </Typography>
-                    <Typography className={'event-section-main-paragraph'}>
-                        {event.locationName}
-                    </Typography>
-                    <Typography className={'event-section-main-paragraph'}>
-                        {event.locationAddress}
-                    </Typography>
-                </>) : (<>
-                    <Typography className={'event-section-main-paragraph'}>
-                        {event.locationAddress}
-                    </Typography>
-                </>)}
+                <Typography className={'event-section-main-paragraph'}>
+                    {event.time}
+                </Typography>
+                {event.locationName.length > 0 && (<Typography className={'event-section-main-paragraph'}>
+                    {event.locationName}
+                </Typography>)}
+                <Typography className={'event-section-main-paragraph'}>
+                    {event.locationAddress}
+                </Typography>
                 <Typography className={'event-section-main-paragraph'}>
                     {event.attire}
                 </Typography>
-                {(i < dict.events.length - 1) && <hr />}
+                {(i < dict.events.length - 1) && <hr/>}
             </div>))}
         </section>
     )
